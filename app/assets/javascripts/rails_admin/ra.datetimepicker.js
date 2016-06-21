e6c4d5cd696bc709910c08f6e0e0606208d892ff
praveenmenon <pmenon@qwinixtech.com>
2016-06-21 18:03:09 +0530

    [devfix_date_format]checks issue with datepicker format

--- app/assets/javascripts/rails_admin/ra.datetimepicker.js
+++ app/assets/javascripts/rails_admin/ra.datetimepicker.js
@@ -13,59 +13,60 @@
 */
 (function($) {
 
-	$.widget("ra.datetimepicker", {
-		options: {
-			showDate: true,
-			showTime: true,
-			datepicker: {},
-			timepicker: {}
-		},
+  $.widget("ra.datetimepicker", {
+    options: {
+      showDate: true,
+      showTime: true,
+      datepicker: {},
+      timepicker: {}
+    },
 
-		_create: function() {
-			var widget = this;
-			this.element.hide();
+    _create: function() {
+      var widget = this;
+      this.element.hide();
 
-			if (this.options.showTime) {
-				this.timepicker = $('<input class="TIMEPICKER" type="text" value="' + this.options.timepicker.value + '" />');
+      if (this.options.showTime) {
+        this.timepicker = $('<input class="TIMEPICKER" type="text" value="' + this.options.timepicker.value + '" />');
 
-				this.timepicker.css("width", "60px");
+        this.timepicker.css("width", "60px");
 
-				this.timepicker.insertAfter(this.element);
+        this.timepicker.insertAfter(this.element);
 
-				this.timepicker.bind("change", function() { widget._onChange(); });
+        this.timepicker.bind("change", function() { widget._onChange(); });
 
-				this.timepicker.timepicker(this.options.timepicker);
-			}
+        this.timepicker.timepicker(this.options.timepicker);
+      }
 
-			if (this.options.showDate) {
-				this.datepicker = $('<input type="text" value="' + this.options.datepicker.value + '" />');
+      if (this.options.showDate) {
+        this.datepicker = $('<input type="text" value="' + this.options.datepicker.value + '" />');
 
-				this.datepicker.css("margin-right", "10px");
+        this.datepicker.css("margin-right", "10px");
 
-				this.datepicker.insertAfter(this.element);
+        this.datepicker.insertAfter(this.element);
 
-				this.datepicker.bind("change", function() { widget._onChange(); });
+        this.datepicker.bind("change", function() { widget._onChange(); });
 
-				this.datepicker.datepicker(this.options.datepicker);
-			}
-		},
+        this.datepicker.datepicker(this.options.datepicker);
+      }
+    },
 
-		_onChange: function() {
-			var value = [];
+    _onChange: function() {
+      var value = [];
 
+      if (this.options.showDate) {
+        locale = "en-us",
+        date = this.datepicker.val();
+        console.log(date);
+        var objDate = new Date(date);
+        value.push(objDate.toLocaleString(locale, { month: "long", day: 'numeric', year: 'numeric'}));
+        console.log(value);
+      }
 
-			if (this.options.showDate) {
-				locale = "en-us",
-				date = this.datepicker.val();
-				var objDate = new Date(date);
-				value.push(objDate.toLocaleString(locale, { month: "long", day: 'numeric', year: 'numeric'}));
-			}
+      if (this.options.showTime) {
+        value.push(this.timepicker.val());
+      }
 
-			if (this.options.showTime) {
-				value.push(this.timepicker.val());
-			}
-
-			this.element.val(value.join(" "));
-		}
-	});
+      this.element.val(value.join(" "));
+    }
+  });
 })(jQuery);
